0f8af5f1c438b1ab00b3e0861092559c34e89825
Julia Martins <mjuh.ms@gmail.com>
2022-07-04 17:48:29 -0300

    Add `/tasks` route Add `/tasks/save` route Add `/tasks/delete` route

--- tasks/TasksController.js
+++ tasks/TasksController.js
@@ -1,8 +1,68 @@
 const express = require('express')
 const router = express.Router()
+const Task = require('./Task')
 
-router.get('/app', (req, res) => {
-  res.render('index.ejs')
+router.get('/tasks', async(req, res) => {
+  try {
+    const tasksDB = await Task.findAll({
+      where: {
+        active: true
+      },
+      order: [
+        ['priority']
+      ]
+    })
+
+    const tasks = tasksDB
+      .map(task => task.toJSON())
+
+    res.render('index.ejs', { tasks })
+
+  } catch (error) {
+    console.error('>>> Erro ao buscar tarefas:', error)
+  }
+})
+
+router.post('/tasks/save', async(req, res) => {
+  const description = req.body.task
+
+  if(description){
+    try {
+      // TODO: Passar o id do usuário correto
+      await Task
+        .create({
+          description,
+          userId: 1
+        })
+
+      res.redirect('/tasks')
+    } catch (error) {
+      console.error('>>> Erro na criação de tarefa:', error)
+    }
+  }
+})
+
+router.post('/tasks/delete', async(req, res) => {
+  const id = req.body.id
+
+  if(!isNaN(id)){
+    try {
+      const taskDB = await Task
+        .findOne({
+          where: { id }
+        })
+
+      await taskDB
+        .update({
+          active: false
+         })
+
+      res.redirect('/tasks')
+
+    } catch (error) {
+        console.error('>>> Erro ao inativar tarefa:', error)
+    }
+  }
 })
 
 module.exports = router;
